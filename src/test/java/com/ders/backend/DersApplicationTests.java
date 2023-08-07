33f7b1f1748bdcb7512af5f118d867438255adca
Akash Ramesh <akash.rjb@gmail.com>
2023-08-07 21:57:27 +0530

    Initial Commit: Project Structure

---
+++ src/test/java/com/ders/backend/DersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ders.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

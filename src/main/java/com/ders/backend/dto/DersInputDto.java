3fda500ea2b0db6c8318efe3dd2e6b6192b6559a
batunii <sy.soni@live.com.sg>
2023-08-12 18:39:41 +0530

    Added JESI integration
    Signed-off-by: batunii <sy.soni@live.com.sg>

--- src/main/java/com/ders/backend/dto/DersInputDto.java
+++ src/main/java/com/ders/backend/dto/DersInputDto.java
@@ -8,4 +8,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class DersInputDto {
     String searchWords;
+
+    public String getSearchWords() {
+        return searchWords;
+    }
 }
--- src/main/java/com/ders/backend/services/DersServiceImpl.java
+++ src/main/java/com/ders/backend/services/DersServiceImpl.java
@@ -1,28 +1,32 @@
 package com.ders.backend.services;
 
+import com.ders.backend.configs.JesiConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class DersServiceImpl implements DersService{
 
+    @Autowired
+    JesiConfig jesi;
     List<String> wordList = new ArrayList<>(Arrays.asList(" for "," in "," the "," a "," on "," at "," of "));
 
     String cleanedWords;
 
     @Override
-    public String searchResume(String searchWords){
-        try{
-            cleanedWords = searchWords;
-            for(String word:wordList){
-                cleanedWords = cleanedWords.replace(word," ");
-            }
-            return cleanedWords;
-        }catch(RuntimeException runtimeException){
-            return "Runtime Exception has occured";
+    public Map<String,Double> searchResume(String searchWords){
+        cleanedWords = searchWords;
+        for(String word:wordList){
+            cleanedWords = cleanedWords.replace(word," ");
         }
+        return jesi.searchJesi(cleanedWords);
+    }
+
+    @Override
+    public String indexFiles(String filePath) {
+        jesi = new JesiConfig(filePath);
+        return "Files Indexed";
     }
 }

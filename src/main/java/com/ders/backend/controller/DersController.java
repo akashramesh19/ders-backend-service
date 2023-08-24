d8c6d8def7e73026bf22621687882fe9b27b188b
Akash Ramesh <akash.rjb@gmail.com>
2023-08-24 23:34:18 +0530

    Added Loggers

--- src/main/java/com/ders/backend/controller/DersController.java
+++ src/main/java/com/ders/backend/controller/DersController.java
@@ -2,6 +2,8 @@ package com.ders.backend.controller;
 
 import com.ders.backend.dto.DersInputDto;
 import com.ders.backend.services.DersServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +18,14 @@ public class DersController {
     @Autowired
     DersServiceImpl dersService;
 
+    Logger logger = LogManager.getLogger(DersController.class);
+
     @PostMapping("/search")
     public ResponseEntity<TreeMap<String,String>> searchTokens(@RequestBody DersInputDto dersInputDto){
         try{
-            dersService.searchResume(dersInputDto.getSearchWords());
+            logger.info("DERS received the following user input: {}",dersInputDto.getSearchWords());
+            String cleanedWords = dersService.searchResume(dersInputDto.getSearchWords());
+            logger.info("DERS is calling JESI to search for the following words");
             return new ResponseEntity<>(new TreeMap<>(),HttpStatus.OK);
         }catch (RuntimeException runtimeException){
             return new ResponseEntity<>(new TreeMap<>(),HttpStatus.BAD_GATEWAY);
--- src/main/java/com/ders/backend/services/DersServiceImpl.java
+++ src/main/java/com/ders/backend/services/DersServiceImpl.java
@@ -1,5 +1,8 @@
 package com.ders.backend.services;
 
+import com.ders.backend.controller.DersController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,6 +12,8 @@ import java.util.List;
 @Service
 public class DersServiceImpl implements DersService{
 
+    Logger logger = LogManager.getLogger(DersServiceImpl.class);
+
     List<String> wordList = new ArrayList<>(Arrays.asList(" for "," in "," the "," a "," on "," at "," of "));
 
     String cleanedWords;
@@ -16,6 +21,7 @@ public class DersServiceImpl implements DersService{
     @Override
     public String searchResume(String searchWords){
         try{
+            logger.info("DERS is cleaning the user input");
             cleanedWords = searchWords;
             for(String word:wordList){
                 cleanedWords = cleanedWords.replace(word," ");
